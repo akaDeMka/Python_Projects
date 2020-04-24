5acde90a1eb70112321b23ba1ccfb58a02c71f2e
Dzmitry.Shchadzinski <demitr@tut.by>
2020-04-24 20:55:38 +0300

    Add exception to lesson 4

--- lesson_4/Calc_Imt.py
+++ lesson_4/Calc_Imt.py
@@ -1,7 +1,11 @@
 from decimal import Decimal
 user_weight=input("Введите ваш вес, кг: ")
 user_height=input("Введите ваш рост, см: ")
-user_height=Decimal(user_height)/100
-user_weight=Decimal(user_weight)
+try:
+    user_height=Decimal(user_height)/100
+    user_weight=Decimal(user_weight)
+except: 
+    print("Вы ввели неверные значения параметров тела")
+    exit()
 imt=user_weight/(user_height*user_height)
 print("Ваш индекс тела -",imt.quantize(Decimal("1.00")))
