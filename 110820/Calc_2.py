e912dcc43c42ddc87ab54e6d56aebadeda3b857a
Dzmitry.Shchadzinski <demitr@tut.by>
2020-05-10 12:27:17 +0300

    Calc 2.2

--- 110820/Calc_2.py
+++ 110820/Calc_2.py
@@ -1,10 +1,10 @@
 from decimal import Decimal                                                                 #Импортируем для нормальных расчетов с дробными числами
 import os   
 
-def print_users(list):                                      #Процедура для вывода пользователей
-    element=len(list)
-    for i in range(0,element,1):
-        print(i+1,' '+list[i]['ФИО'])
+def print_users():
+    global users_database                                      #Процедура для вывода пользователей
+    for i in range(0,len(users_database)-1,1):
+        print(i+1,' '+users_database[i]['ФИО'])
 
 def print_user_data(list_opt):
     print("1. ФИО: ",list_opt['ФИО'])
@@ -13,8 +13,9 @@ def print_user_data(list_opt):
     print("4. Вес: ",list_opt['Вес'],' кг')
     print("5. Рост: ",list_opt['Рост'], ' см')
 
-def get_user(list):                                         #Функция для выбора пользователя из списка
-    print_users(list)
+def get_user():                                         #Функция для выбора пользователя из списка
+    global users_database
+    print_users()
     while True:
         key=input('Введите номер пользователя: ')
         try:
@@ -22,13 +23,13 @@ def get_user(list):                                         #Функция дл
         except:
             print('Вы ввели неверное значение')
         else:
-            if int(key) in range(1,len(list)+1,1):
+            if int(key) in range(1,len(users_database),1):
                 return int(key)-1
                 break
             else:
                 print('Вы ввели неверное значение')
 
-def get_user_data(user_data,position):
+def get_user_data(user_data,position=0):
     if not position or position==1:
         user_data['ФИО']=input('\nВведите ФИО: ')
     if not position or position==2:
@@ -67,8 +68,8 @@ def get_user_data(user_data,position):
                 break
     return user_data
 
-def main():
-    imt_list = [                                                                                #Расшифровка ИМТ
+def imt_calc(list):
+    imt_list = [                                                                        #Расшифровка ИМТ
     [16, 'выраженный дефицит массы тела.', 'Пора начинать кушать или боюсь скоро вы окажетесь в больнице! Прекращаем голодать!'],
     [18, 'недостаточная (дефицит) масса тела', 'Питайтесь больше! Все еще недостаточное питание '],
     [25, 'норма', 'Все хорошо! Так держать!'],
@@ -77,88 +78,96 @@ def main():
     [40, 'ожирение резкое', 'Снижение калорий в 2-3 раза строго рекомендуется!'],
     [100, 'очень резкое ожирение', 'Если вы еще можете ходить - срочно к врачу! Ну и кушаем в основном воду (хлеб отменяется)']
     ]
-    key='y'
-    users=0                                                     #Счетчик, определяющий количество пользователей в базе 
+    user_height=Decimal(list['Рост'])/100                    
+    user_weight=Decimal(list['Вес'])            
+    imt=user_weight/(user_height*user_height)                                           #Расчеты
+    imt=imt.quantize(Decimal("1.00"))
+    if imt>100 or imt<0:                                                                #Проверяем что значения не выходят за границы 0-100
+        print("\nВам срочно стоит обратится к врачу - ваше состояние не нормальное")  
+    else:    
+        print("\n    Ваш индекс тела -", imt)
+        imt=int(imt.quantize(Decimal("1")))                                             #Округление и приведение типов
+        print('\n    0 '+(imt-1)*u'―'+'■'+(100-imt)*'―'+' 100')                         #Вывод Визуальной строки :)
+        for key in imt_list:                                                            #Вывод интерпритации индекса массы тела
+            if imt<=key[0] :
+                print('\n    У вас',key[1],'\n', '   Наш совет: ',key[2])
+                break
+
+def delete_user(position):
+    global users_database
+    key=input("\nВы уверены? (y)")
+    if key=='y' or key=='н':
+        del users_database[position]
+        print("\nПользователь удален\n\nТекущий список пользователей")
+        print_users()
+    else:
+        print("\nУдаление отменено")
+
+def change_user_data(list):
+    print("\nВыберите какие данные вы хотите изменить:")
+    print_user_data(list)
+    key=input("\nВведите номер записи, которую хотите изменить. Для выхода введите любую другую клавишу: ")
+    try:
+        key=int(key)
+    except:
+        print("Выход без изменений")
+    else:
+        list=get_user_data(list,key)
+        print('\nИзмененные данные:')
+        print_user_data(list)
+    return list
+
+def create_user():
+    global users_database
+    users=len(users_database)-1
+    print('Создание нового пользователя\nВведите ФИО: ')
+    users_database[users]=get_user_data(users_database[users])
+    print('\nВведенные данные: ')
+    print_user_data(users_database[users])
+    users_database.append({})
+    return True   
+
+def print_menu(users):
+    if users:                                               #чтобы окно сразу не очищалось и было видно результат
+        key=input("\nДля продолжения введите любую клавишу ")                                 
+    os.system('cls' if os.name == 'nt' else 'clear')        #Очистка экрана
+    print("Операции:\n")
+    print("1. Ввести нового пользователя")
+    if users:                                               #блокируем вывод меню, которое недоступно, пока нет пользователей
+        print("2. Изменить данные пользователя")
+        print("3. Рассчитать ИМТ для пользователя")
+        print("4. Вывести список пользователей")
+        print("5. Удалить пользователя")
+    print("q - Выход")
+    key=input("\nЧто еще хотите сделать? (Введите 'q' для выхода из программы): ") 
+    os.system('cls' if os.name == 'nt' else 'clear')        #Очистка экрана
+    return key
+
+def main():
+    global users_database
+    key='y'                                                     
+    users=False
 
     while key!='q' and key!='й':                                #тело программы в цикле
-        if users:                                               #чтобы окно сразу не очищалось и было видно результат
-            key=input("\nДля продолжения введите любую клавишу ")                                 
-        os.system('cls' if os.name == 'nt' else 'clear')        #Очистка экрана
-        print("Операции:\n")
-        print("1. Ввести нового пользователя")
-        if users:                                               #блокируем вывод меню, которое недоступно, пока нет пользователей
-            print("2. Изменить данные пользователя")
-            print("3. Рассчитать ИМТ для пользователя")
-            print("4. Вывести список пользователей")
-            print("5. Удалить пользователя")
-        print("q - Выход")
-        key=input("\nЧто еще хотите сделать? (Введите 'q' для выхода из программы): ") 
-        os.system('cls' if os.name == 'nt' else 'clear')        #Очистка экрана
-
+        key=print_menu(users)                                       #Вывести меню
+        
         if key=="1":                                            #1. Ввести нового пользователя
-            if users:                                          
-                user_list.append({})
-            else:
-                user_list = [{}]
-            print('Создание нового пользователя\nВведите ФИО: ')
-            user_list[users]=get_user_data(user_list[users],0)
-            print('\nВведенные данные: ')
-            print_user_data(user_list[users])
-            users=len(user_list)
+            users=create_user()
 
         elif key=='2' and users:                                #2. Изменить данные пользователя
-            user_got=get_user(user_list)
-            if user_got==-1:
-                print('Такого пользователя нет')
-            else:
-                print("\nВыберите какие данные вы хотите изменить:")
-                print_user_data(user_list[user_got])
-                print("6. Для выхода введите любую другую клавишу")
-                key=input("\nВведите номер записи, которую хотите изменить (Введите 'q' для выхода без изменений): ")
-                try:
-                    key=int(key)
-                except:
-                    print("Вы ввели неверное значение ключа")
-                    key=22
-                else:
-                    user_list[user_got]=get_user_data(user_list[user_got],key)
-                    print('\nИзмененные данные:')
-                    print_user_data(user_list[user_got])    
+            got_user=get_user()                                
+            users_database[got_user]=change_user_data(users_database[got_user])     
         
         elif key=='3'and users:                                 #3. Рассчитать ИМТ для пользователя
-            user_got=get_user(user_list)
-            user_height=Decimal(user_list[user_got]['Рост'])/100                    
-            user_weight=Decimal(user_list[user_got]['Вес'])            
-            imt=user_weight/(user_height*user_height)                                           #Расчеты
-            imt=imt.quantize(Decimal("1.00"))
-            if imt>100 or imt<0:                                                                #Проверяем что значения не выходят за границы 0-100
-                print("\nВам срочно стоит обратится к врачу - ваше состояние не нормальное")  
-            else:    
-                print("\n    Ваш индекс тела -", imt)
-                imt=int(imt.quantize(Decimal("1")))                                             #Округление и приведение типов
-                print('\n    0 '+(imt-1)*u'―'+'■'+(100-imt)*'―'+' 100')                         #Вывод Визуальной строки :)
-                for key in imt_list:                                                            #Вывод интерпритации индекса массы тела
-                    if imt<=key[0] :
-                        print('\n    У вас',key[1],'\n', '   Наш совет: ',key[2])
-                        break
+            imt_calc(users_database[get_user()])
 
         elif key=='4'and users:                                 #4. Вывести список пользователей
-            user_got=get_user(user_list)
-            if user_got>-1:
-                print_user_data(user_list[user_got])
-            else:
-                print("Такого пользователя нет")                        
+            print_user_data(users_database[get_user()])                     
 
         elif key=='5'and users:                                 #5. Удалить пользователя
-            user_got=get_user(user_list)
-            key=input("\nВы уверены? (y)")
-            if key=='y' or key=='н':
-                user_list.pop(user_got,None)
-                print("\nПользователь удален\n\nТекущий список пользователей")
-                print_users(user_list)
-            else:
-                print("\nУдаление отменено")
-                key=11
+            delete_user(get_user())
+
+users_database=[{}]  
 
 if __name__ == "__main__":
     main()
