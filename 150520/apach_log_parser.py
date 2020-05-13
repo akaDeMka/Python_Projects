8d5978bfd6eeca1aa9046ae4d1ea66096c15c52c
Dzmitry.Shchadzinski <demitr@tut.by>
2020-05-13 23:00:34 +0300

    parser 1.2.3 path experements

--- 150520/apach_log_parser.py
+++ 150520/apach_log_parser.py
@@ -31,7 +31,7 @@ def check_browser(browser):
     print('Всего записей с браузером ',browser,' - ',count)
 
 def main():
-    load_data(input('Текущая диектория '+os.path.abspath(".")+' Введите путь к файлу: '))      #Можно ввести вручную - но лень
+    load_data(input('Текущая диектория '+os.path.abspath(".")+'\nВведите путь к файлу: '))      #Можно ввести вручную - но лень
     #load_data('150520/apache_logs.txt') #Тут в зависимостьи от стартовой директории - я запускаю так
     os.system('cls' if os.name == 'nt' else 'clear') 
     count_records()
