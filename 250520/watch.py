e902ac00e7cdac708bc8a006f0542bba01836004
Dzmitry.Shchadzinski <demitr@tut.by>
2020-05-24 19:08:05 +0300

    Watch 1.3.0 with dll

--- 250520/watch.py
+++ 250520/watch.py
@@ -81,33 +81,26 @@ delimiters=[
 
 def print_watch(hours,minutes,seconds):
     global digits,delimiters
-    try:
-        hours=int(hours)
-        minutes=int(minutes)
-        seconds=int(seconds)
-    except:
-        print('Введены неверные данные')
+    if not seconds%2:
+        delim=1
     else:
-        if not seconds%2:
-            delim=1
-        else:
-            delim=0
-        if hours>9:
-            hour_1=hours//10
-        else:
-            hour_1=0
-        hours=hours%10
-        if minutes>9:
-            minute_1=minutes//10
-        else:
-            minute_1=0
-        minutes=minutes%10
-        if seconds>9:
-            second_1=seconds//10
-        else:
-            second_1=0
-        seconds=seconds%10
-        for x in range(0,5,1):
-            print(digits[hour_1][x]+delimiters[2][x]+digits[hours][x]+delimiters[delim][x]\
-            +digits[minute_1][x]+delimiters[2][x]+digits[minutes][x]+delimiters[delim][x]\
-            +digits[second_1][x]+delimiters[2][x]+digits[seconds][x])
+        delim=0
+    if hours>9:
+        hour_1=hours//10
+    else:
+        hour_1=0
+    hours=hours%10
+    if minutes>9:
+        minute_1=minutes//10
+    else:
+        minute_1=0
+    minutes=minutes%10
+    if seconds>9:
+        second_1=seconds//10
+    else:
+        second_1=0
+    seconds=seconds%10
+    for x in range(0,5,1):
+        print(digits[hour_1][x]+delimiters[2][x]+digits[hours][x]+delimiters[delim][x]\
+        +digits[minute_1][x]+delimiters[2][x]+digits[minutes][x]+delimiters[delim][x]\
+        +digits[second_1][x]+delimiters[2][x]+digits[seconds][x])
