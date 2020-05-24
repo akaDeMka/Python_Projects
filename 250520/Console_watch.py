21710bac3aca64db6f6a9b1c3f9b7a5c4418b87d
Dzmitry.Shchadzinski <demitr@tut.by>
2020-05-24 17:17:18 +0300

    Watch 1.1.2 with dll

--- 250520/Console_watch.py
+++ 250520/Console_watch.py
@@ -4,10 +4,14 @@ from datetime import datetime
 import tkinter
 import watch
 
-root=tkinter.Tk()
-while True:
-    os.system('cls' if os.name == 'nt' else 'clear') 
-    watch.print_watch(int(datetime.now().hour),int(datetime.now().minute),int(datetime.now().second))
-    print('To exit press Ctrl+C buttons')
-    root.after(100)
-    #time.sleep(0.1)
+def main():
+    root=tkinter.Tk()
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear') 
+        watch.print_watch(int(datetime.now().hour),int(datetime.now().minute),int(datetime.now().second))
+        print('To exit press Ctrl+C buttons')
+        root.after(100)
+        #time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
