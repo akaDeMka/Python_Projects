4e60a43d74ee60a710d8ff2e36590b91e91b6cf1
Dzmitry.Shchadzinski <demitr@tut.by>
2020-05-24 17:42:14 +0300

    Watch 1.2 with dll

--- 250520/Console_watch.py
+++ 250520/Console_watch.py
@@ -8,7 +8,7 @@ def main():
     root=tkinter.Tk()
     while True:
         os.system('cls' if os.name == 'nt' else 'clear') 
-        watch.print_watch(int(datetime.now().hour),int(datetime.now().minute),int(datetime.now().second))
+        watch.print_watch(datetime.now().hour, datetime.now().minute, datetime.now().second)
         print('To exit press Ctrl+C buttons')
         root.after(100)
         #time.sleep(0.1)
--- 250520/watch.py
+++ 250520/watch.py
@@ -49,13 +49,13 @@ digits=[
      '⬜⬜⬜⬜',
      '⬛⬛⬛⬜',
      '⬜⬜⬜⬜'],
-]
+    ]
 delimiters=[
     ['⬛⬛⬛',
      '⬛⬛⬛',
      '⬛⬛⬛',
      '⬛⬛⬛',
-     '⬛⬛⬛'],
+    '⬛⬛⬛'],
     ['⬛⬛⬛',
      '⬛⬜⬛',
      '⬛⬛⬛',
@@ -66,30 +66,37 @@ delimiters=[
      '⬛',
      '⬛',
      '⬛'],
-]
+    ]
 
 def print_watch(hours,minutes,seconds):
     global digits,delimiters
-    if not seconds%2:
-        delim=1
+    try:
+        hours=int(hours)
+        minutes=int(minutes)
+        seconds=int(seconds)
+    except:
+        print('Введены неверные данные')
     else:
-        delim=0
-    if hours>9:
-        hour_1=hours//10
-    else:
-        hour_1=0
-    hours=hours%10
-    if minutes>9:
-        minute_1=minutes//10
-    else:
-         minute_1=0
-    minutes=minutes%10
-    if seconds>9:
-        second_1=seconds//10
-    else:
-        second_1=0
-    seconds=seconds%10
-    for x in range(0,5,1):
-        print(digits[hour_1][x]+delimiters[2][x]+digits[hours][x]+delimiters[delim][x]\
-        +digits[minute_1][x]+delimiters[2][x]+digits[minutes][x]+delimiters[delim][x]\
-        +digits[second_1][x]+delimiters[2][x]+digits[seconds][x])
+        if not seconds%2:
+            delim=1
+        else:
+            delim=0
+        if hours>9:
+            hour_1=hours//10
+        else:
+            hour_1=0
+        hours=hours%10
+        if minutes>9:
+            minute_1=minutes//10
+        else:
+            minute_1=0
+        minutes=minutes%10
+        if seconds>9:
+            second_1=seconds//10
+        else:
+            second_1=0
+        seconds=seconds%10
+        for x in range(0,5,1):
+            print(digits[hour_1][x]+delimiters[2][x]+digits[hours][x]+delimiters[delim][x]\
+            +digits[minute_1][x]+delimiters[2][x]+digits[minutes][x]+delimiters[delim][x]\
+            +digits[second_1][x]+delimiters[2][x]+digits[seconds][x])
